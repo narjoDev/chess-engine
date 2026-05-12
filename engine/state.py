4f0cc8560888a74556dd6252e0e82b3bdc884fbd
narjoDev <63086420+narjoDev@users.noreply.github.com>
2026-05-11 18:21:22 -0700

    square class and function placeholders

--- engine/state.py
+++ engine/state.py
@@ -1,6 +1,45 @@
 from enum import Enum
 
 
+class Color(Enum):
+    WHITE = "WHITE"
+    BLACK = "BLACK"
+
+
+class PieceType(Enum):
+    KING = "KING"
+    QUEEN = "QUEEN"
+    ROOK = "ROOK"
+    BISHOP = "BISHOP"
+    KNIGHT = "KNIGHT"
+    PAWN = "PAWN"
+
+
+class File(Enum):
+    A = "A"
+    B = "B"
+    C = "C"
+    D = "D"
+    E = "E"
+    F = "F"
+    G = "G"
+    H = "H"
+
+
+class Square:
+    def __init__(self, file: File, rank: int):
+        assert rank >= 1 and rank <= 8
+        self.file = file
+        self.rank = rank
+
+
+class Piece:
+    def __init__(self, color, piece_type):
+        self.color: Color = color
+        self.piece_type: PieceType = piece_type
+        self.has_moved: bool = False
+
+
 class GameState:
     def __init__(self):
         self.board: dict[tuple[str, int], Piece] = {
@@ -24,23 +63,23 @@ class GameState:
             ("h", 8): Piece(Color.BLACK, PieceType.ROOK),
         }
 
+    def is_move_legal(self, move) -> bool:
+        # can piece move like that
+        # is piece blocking or in between
+        # will mover's king enter check (handles pins)
+        pass
 
-class Color(Enum):
-    WHITE = "WHITE"
-    BLACK = "BLACK"
+    def is_color_in_check(self, color: Color) -> bool:
+        pass
 
+    def is_color_in_checkmate(self, color: Color) -> bool:
+        pass
 
-class PieceType(Enum):
-    KING = "KING"
-    QUEEN = "QUEEN"
-    ROOK = "ROOK"
-    BISHOP = "BISHOP"
-    KNIGHT = "KNIGHT"
-    PAWN = "PAWN"
+    def get_piece_attacked_squares(self, piece) -> list[Square]:
+        # does not filter attacked squares (ignores other pieces)
+        pass
 
-
-class Piece:
-    def __init__(self, color, piece_type):
-        self.color: Color = color
-        self.piece_type: PieceType = piece_type
-        self.has_moved: bool = False
+    def get_piece_movable_squares(self, piece) -> list[Square]:
+        # includes attacked squares
+        # only different from get_piece_attacked_squares for PAWNS
+        pass
